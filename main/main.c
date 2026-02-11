c99e51f70f0996d792e2122c729123300ca09fac
skobele28 <skobele@union.edu>
2026-02-11 01:08:59 -0500

    comments and simplification

--- main/main.c
+++ main/main.c
@@ -9,19 +9,21 @@
 #include "esp_adc/adc_oneshot.h"
 #include "driver/ledc.h"
 
-#define PSEAT_PIN       GPIO_NUM_7       // passenger seat button pin 7
-#define DSEAT_PIN       GPIO_NUM_5       // driver seat button pin 5
-#define PBELT_PIN       GPIO_NUM_15       // passenger belt switch pin 15
-#define DBELT_PIN       GPIO_NUM_6       // driver belt switch pin 6
-#define IGNITION_BUTTON GPIO_NUM_4 // ignition button pin 4
-#define READY_LED       GPIO_NUM_20      // ready LED pin 20
-#define SUCCESS_LED     GPIO_NUM_19    // success LED pin 19
-#define ALARM_PIN       GPIO_NUM_18       // alarm pin 18
-#define WIPER_CONTROL   ADC_CHANNEL_8 // headlight control (potentiometer) ADC1 channel 8
-#define INT_WIPER_CONTROL      ADC_CHANNEL_9   // LDR sensor (auto headlight) ADC1 channel 0
+// ignition subsystem
+#define PSEAT_PIN       GPIO_NUM_7      // passenger seat button pin 7
+#define DSEAT_PIN       GPIO_NUM_5      // driver seat button pin 5
+#define PBELT_PIN       GPIO_NUM_15     // passenger belt switch pin 15
+#define DBELT_PIN       GPIO_NUM_6      // driver belt switch pin 6
+#define IGNITION_BUTTON GPIO_NUM_4      // ignition button pin 4
+#define READY_LED       GPIO_NUM_20     // ready LED pin 20
+#define SUCCESS_LED     GPIO_NUM_19     // success LED pin 19
+#define ALARM_PIN       GPIO_NUM_18     // alarm pin 18
+
+// wiper subsystem
+#define WIPER_CONTROL   ADC_CHANNEL_8   // wiper control (potentiometer) ADC1 channel 8
+#define INT_WIPER_CONTROL      ADC_CHANNEL_9   // wiper intermittence control (potentiometer) ADC1 channel 9
 #define ADC_ATTEN       ADC_ATTEN_DB_12 // set ADC attenuation
 #define BITWIDTH        ADC_BITWIDTH_12 // set ADC bitwidth
-#define WIPER_MOTOR     GPIO_NUM_16     // signal to motor pin 16
 #define WIPER_POTENT_OFF    (500)       // adcmV level for wipers off
 #define WIPER_POTENT_LOW    (1570)      // adcmV level for wipers low
 #define WIPER_POTENT_HI     (2650)      // adcmV level for wipers high
@@ -30,89 +32,70 @@
 
 #define LEDC_TIMER      LEDC_TIMER_0
 #define LEDC_MODE       LEDC_LOW_SPEED_MODE
-#define LEDC_OUTPUT_IO      (16)
+#define LEDC_OUTPUT_IO      (16)        // pwm signal to motor pin 16
 #define LEDC_CHANNEL    LEDC_CHANNEL_0
-#define LEDC_DUTY_RES   LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY_RES   LEDC_TIMER_13_BIT // set duty resolution to 13 bits
 
 //Set the PWM signal frequency required by servo motor
-#define LEDC_FREQUENCY          (50) // Frequency in Hertz. 
+#define LEDC_FREQUENCY      (50) // Frequency in Hertz. 
 
 //Calculate the values for the minimum (0.75ms) and center (1.5ms) servo pulse widths
-#define LEDC_DUTY_MIN           (210) // Set duty to 3.75%.
-#define LEDC_DUTY_CENTER           (614) // Set duty to 7.5%.
+#define LEDC_DUTY_MIN       (210) // Set duty to ~3.75%.
+#define LEDC_DUTY_CENTER    (614) // Set duty to ~7.5%.
 
-bool dseat = false;  //Detects when the driver is seated 
-bool pseat = false;  //Detects when the passenger is seated
-bool dbelt = false;  //Detects when the driver seatbelt is on
-bool pbelt = false;  //Detects when the passenger seatbelt is on
-bool ignition = false; //Detects when the ignition is turned on
-int executed = 0; //keep track of print statements
-int ready_led = 0; //keep track of whether ready_led should be on or off
-int ignition_off = 0; // keep track of whether the ignition can be turned off
-int wiper = 0;    // keep track of wiper setting
-int wiper_int = 0;  // keep track of wiper intermittent setting
+bool dseat = false;     //Detects when the driver is seated 
+bool pseat = false;     //Detects when the passenger is seated
+bool dbelt = false;     //Detects when the driver seatbelt is on
+bool pbelt = false;     //Detects when the passenger seatbelt is on
+bool ignition = false;  //Detects when the ignition is turned on
+int executed = 0;       //keeps track of print statements
+int ready_led = 0;      //keeps track of whether ready_led should be on or off
+int ignition_off = 0;   //keeps track of whether the ignition can be turned off
+int wiper = 0;          //keeps track of wiper setting
+int wiper_int = 0;      //keeps track of wiper intermittent setting
 
 static void ledc_initialize(void);
 
+// Task to set wipers according to WIPER_CONTROL (potentiometer) and intermittence
 void wiper_task(void *pvParameter)
 {
     while(executed != 3){
+        // if wiper is set to OFF, make motor stationary at minimum angle
         if(wiper == 0){
             ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_MIN);
             ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+            vTaskDelay(10/portTICK_PERIOD_MS);
         }
-
+        
+        // if wiper is set to INT, rotate to 90 degrees and back at low speed
         else if(wiper == 1){
+            int i;
+            int delay = 1500/(LEDC_DUTY_CENTER - LEDC_DUTY_MIN);
+            for(i = LEDC_DUTY_MIN; i <= LEDC_DUTY_CENTER; i = i + 5){
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, i);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                vTaskDelay((delay * 5)/portTICK_PERIOD_MS);
+            }
+            for(i = LEDC_DUTY_CENTER; i >= LEDC_DUTY_MIN; i = i - 5){
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, i);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                vTaskDelay((delay * 5)/portTICK_PERIOD_MS);
+            }
+            // if intermittent SHORT, delay 1 second
             if (wiper_int == 1){
-                int i;
-                int delay = 1500/(LEDC_DUTY_CENTER - LEDC_DUTY_MIN);
-                for(i = LEDC_DUTY_MIN; i <= LEDC_DUTY_CENTER; i = i + 5){
-                    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, i);
-                    ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-                    vTaskDelay((delay * 5)/portTICK_PERIOD_MS);
-                }
-                for(i = LEDC_DUTY_CENTER; i >= LEDC_DUTY_MIN; i = i - 5){
-                    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, i);
-                    ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-                    vTaskDelay((delay * 5)/portTICK_PERIOD_MS);
-                }
                 vTaskDelay(1000/portTICK_PERIOD_MS);
             }
-            
+            // if intermittence MED, delay 3 seconds
             else if (wiper_int == 2){
-                int i;
-                int delay = 1500/(LEDC_DUTY_CENTER - LEDC_DUTY_MIN);
-                for(i = LEDC_DUTY_MIN; i <= LEDC_DUTY_CENTER; i = i + 5){
-                    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, i);
-                    ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-                    vTaskDelay((delay * 5)/portTICK_PERIOD_MS);
-                }
-                for(i = LEDC_DUTY_CENTER; i >= LEDC_DUTY_MIN; i = i - 5){
-                    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, i);
-                    ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-                    vTaskDelay((delay * 5)/portTICK_PERIOD_MS);
-                }
                 vTaskDelay(3000/portTICK_PERIOD_MS);
             }
-        
+            // if intermittence LONG, delay 5 seconds
             else if (wiper_int == 3){
-                int i;
-                int delay = 1500/(LEDC_DUTY_CENTER - LEDC_DUTY_MIN);
-                for(i = LEDC_DUTY_MIN; i <= LEDC_DUTY_CENTER; i = i + 5){
-                    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, i);
-                    ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-                    vTaskDelay((delay * 5)/portTICK_PERIOD_MS);
-                }
-                for(i = LEDC_DUTY_CENTER; i >= LEDC_DUTY_MIN; i = i - 5){
-                    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, i);
-                    ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
-                    vTaskDelay((delay * 5)/portTICK_PERIOD_MS);
-                }
                 vTaskDelay(5000/portTICK_PERIOD_MS);
             }
         }
             
-
+        // if wiper set to LOW, rotate to 90 degrees and back to min at low speed (3s period)
         else if(wiper == 2){
             int i;
             int delay = 1500/(LEDC_DUTY_CENTER - LEDC_DUTY_MIN);
@@ -128,6 +111,7 @@ void wiper_task(void *pvParameter)
             }
         }
 
+        // if wiper set to HIGH, rotate to 90 degrees and back to min at high speed (1.2s period)
         else if (wiper == 3){
             int i;
             int delay = 600/(LEDC_DUTY_CENTER - LEDC_DUTY_MIN);
@@ -142,17 +126,17 @@ void wiper_task(void *pvParameter)
                 vTaskDelay((delay * 10)/portTICK_PERIOD_MS);
             }
         }
-        vTaskDelay(10/portTICK_PERIOD_MS);
+        //vTaskDelay(10/portTICK_PERIOD_MS);
     }
 }
 
 void app_main(void)
 {
 
-    int wiper_adc_bits;                       // potentiometer ADC reading (bits)
-    int wiper_adc_mV;                         // potentiometer ADC reading (mV)
-    int int_wiper_adc_bits;                   // LDR ADC reading (bits)
-    int int_wiper_adc_mV;                     // LDR ADC reading (mV)
+    int wiper_adc_bits;                       // wiper potentiometer ADC reading (bits)
+    int wiper_adc_mV;                         // wiper potentiometer ADC reading (mV)
+    int int_wiper_adc_bits;                   // intermittent potent ADC reading (bits)
+    int int_wiper_adc_mV;                     // intermittent potent ADC reading (mV)
 
 
     // set driver seat pin config to input and internal pullup
@@ -192,6 +176,7 @@ void app_main(void)
     gpio_reset_pin(ALARM_PIN);
     gpio_set_direction(ALARM_PIN, GPIO_MODE_OUTPUT);
 
+    // configure adc oneshot for wiper potent and intermittent potent
     adc_oneshot_unit_init_cfg_t init_config1 = {
         .unit_id = ADC_UNIT_1,
     };                                                  // Unit configuration
@@ -218,6 +203,7 @@ void app_main(void)
     adc_cali_create_scheme_curve_fitting                // Populate cal handle
     (&cali_config, &adc1_cali_chan_handle);
 
+    // configuration structure for lcd
     hd44780_t lcd =
     {
         .write_cb = NULL,
@@ -234,6 +220,7 @@ void app_main(void)
         }
     };
 
+    // initialize lcd
     ESP_ERROR_CHECK(hd44780_init(&lcd));
 
     // Set the LEDC peripheral configuration
@@ -246,16 +233,16 @@ void app_main(void)
     while (1){
         
         adc_oneshot_read
-        (adc1_handle, WIPER_CONTROL, &wiper_adc_bits);              // Read ADC bits (potentiometer)
+        (adc1_handle, WIPER_CONTROL, &wiper_adc_bits);              // Read ADC bits (wiper)
         
         adc_cali_raw_to_voltage
-        (adc1_cali_chan_handle, wiper_adc_bits, &wiper_adc_mV);         // Convert to mV (potentiometer)
+        (adc1_cali_chan_handle, wiper_adc_bits, &wiper_adc_mV);         // Convert to mV (wiper)
 
         adc_oneshot_read
-        (adc1_handle, INT_WIPER_CONTROL, &int_wiper_adc_bits);           // Read ADC bits (LDR)
+        (adc1_handle, INT_WIPER_CONTROL, &int_wiper_adc_bits);           // Read ADC bits (wiper int)
         
         adc_cali_raw_to_voltage
-        (adc1_cali_chan_handle, int_wiper_adc_bits, &int_wiper_adc_mV); // Convert to mV (LDR)
+        (adc1_cali_chan_handle, int_wiper_adc_bits, &int_wiper_adc_mV); // Convert to mV (wiper int)
 
 
         // Task Delay to prevent watchdog
@@ -318,7 +305,7 @@ void app_main(void)
                     if (!dbelt){
                         printf("Drivers seatbelt not fastened.\n");
                     }
-                    executed = 4;
+                    executed = 4;   // set executed = 4 so messages print only once
                 
             }
         }
@@ -329,12 +316,16 @@ void app_main(void)
             executed = 1;
         }
 
-        // if iginition successful, set low beams according to potentiometer
+        // if iginition successful, set wipers according to potentiometers
         if(executed == 2){
+            // print "Wipers: " on LCD screen, line 1
             hd44780_gotoxy(&lcd, 0, 0);
             hd44780_puts(&lcd, "Wipers: ");
+
+            // create wiper task
             xTaskCreate(wiper_task, "Wiper_Task", 2048, NULL, 5, NULL);
-            // if potentiometer set to off, set low beam leds to off, set lowbeam = 0
+
+            // if potentiometer set to off, write "wipers: off" on LCD, set wiper = 0
             if(wiper_adc_mV < WIPER_POTENT_OFF){
                 hd44780_gotoxy(&lcd, 8, 0);
                 hd44780_puts(&lcd, "OFF ");
@@ -342,34 +333,35 @@ void app_main(void)
                 hd44780_puts(&lcd, "          ");
                 wiper = 0;
             }
-            // if potentiometer set to auto, use LDR to determine led high/low
+            
+            // if potentiometer set to int, write "wipers: int" on LCD, set wiper = 1
             else if(wiper_adc_mV >= WIPER_POTENT_OFF && wiper_adc_mV < WIPER_POTENT_LOW){
                 hd44780_gotoxy(&lcd, 8, 0);
                 hd44780_puts(&lcd, "INT  ");
                 wiper = 1;
-                // if LDR high mV (daylight) for 2s, turn off low beams, set lowbeam = 0
+                // if int short, write "int: short" on LCD, set wiper_int = 1
                 if (int_wiper_adc_mV < WIPER_INT_SHORT){
                     hd44780_gotoxy(&lcd, 0, 1);
                     hd44780_puts(&lcd, "INT: SHORT");
                     wiper_int = 1;
                     }
                 
-                // if LDR low mV (dusk/night) for 1s, turn on low beams, set lowbeam = 1
+                // if int medium, write "int: med" on LCD, set wiper_int = 2
                 else if (int_wiper_adc_mV >= WIPER_INT_SHORT && int_wiper_adc_mV < WIPER_INT_LONG){
                     hd44780_gotoxy(&lcd, 0, 1);
                     hd44780_puts(&lcd, "INT: MED  ");
                     wiper_int = 2;
                     }
-                        
+                
+                // if int long, write "int: long" on LCD, set wiper_int = 3
                 else if (int_wiper_adc_mV >= WIPER_INT_LONG){
                     hd44780_gotoxy(&lcd, 0, 1);
                     hd44780_puts(&lcd, "INT: LONG  ");
                     wiper_int = 3;
                     }
-                
             }
                 
-            // if potentiometer set to on, turn on low beams, set lowbeam = 1
+            // if wipers set to low, write "wipers: low" on LCD, set wiper = 2
             else if(wiper_adc_mV >= WIPER_POTENT_LOW && wiper_adc_mV < WIPER_POTENT_HI){
                 hd44780_gotoxy(&lcd, 8, 0);
                 hd44780_puts(&lcd, "LOW ");
@@ -378,13 +370,13 @@ void app_main(void)
                 wiper = 2;
             }
 
+            // if wipers set to high, write "wipers: high" on LCD, set wiper = 3
             else if(wiper_adc_mV >= WIPER_POTENT_HI){
                 hd44780_gotoxy(&lcd, 8, 0);
                 hd44780_puts(&lcd, "HIGH");
                 hd44780_gotoxy(&lcd, 0, 1);
                 hd44780_puts(&lcd, "          ");
-                wiper = 3;
-                            
+                wiper = 3;      
             }
         }
 
@@ -399,7 +391,7 @@ void app_main(void)
         if (ignition_off==1 && ignition == true){
             gpio_set_level(SUCCESS_LED,0);          // turn off ignition
             hd44780_clear(&lcd);                    // turn off wiper lcd
-            executed = 3;                           // set executed = 3 to keep LEDs off
+            executed = 3;                           // set executed = 3 to keep LEDs off, exit wiper task loop
         }
     }
 }
